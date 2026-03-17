68f57f9244329a640550c6f0aa1c67819d6a5d38
Rizabekus <rizabek.zhampeisov@gmail.com>
2026-03-18 04:06:18 +0500

    feat: add initial configs

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,16 +3,20 @@ package main
 import (
 	"log"
 	"net"
+	"os"
 
 	"github.com/Rizabekus/shipment-grpc-service/internal/application"
 	"github.com/Rizabekus/shipment-grpc-service/internal/infrastructure/storage"
 	handler "github.com/Rizabekus/shipment-grpc-service/internal/transport/grpc"
 	pb "github.com/Rizabekus/shipment-grpc-service/proto"
+	"github.com/joho/godotenv"
 	"google.golang.org/grpc"
 )
 
 func main() {
-
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, using system environment")
+	}
 	shipmentRepo := storage.NewInMemoryShipmentRepo()
 	eventRepo := storage.NewInMemoryEventRepo()
 
@@ -24,7 +28,7 @@ func main() {
 
 	pb.RegisterShipmentServiceServer(grpcServer, shipmentHandler)
 
-	listener, err := net.Listen("tcp", ":50051")
+	listener, err := net.Listen("tcp", ":"+os.Getenv("GRPC_PORT"))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
